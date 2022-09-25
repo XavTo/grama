5db29daa8a6727e7e08b153aecf5e17a267f9dbd
Xavier <xaviertonnellier6@outlook.fr>
2022-09-26 00:43:47 +0200

    [ADD] : good navigation and proto profil

--- app/src/main/java/fr/x/grama/SettingsActivity.kt
+++ app/src/main/java/fr/x/grama/SettingsActivity.kt
@@ -1,19 +1,14 @@
 package fr.x.grama
 
-import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import fr.x.grama.fragments.ProfFragment
 import fr.x.grama.fragments.SettingFragment
 
 class SettingsActivity : AppCompatActivity() {
-    private var username: String = ""
-    private var password: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         val extra = intent.extras
         val id = extra!!.getInt("id")
@@ -25,16 +20,6 @@ class SettingsActivity : AppCompatActivity() {
         else
             loadFragment(ProfFragment())
         setupCross(tagFragment)
-    }
-
-    private fun setupConnexion() {
-        val pseudoText = findViewById<TextView>(R.id.prof_pseudo)
-        val passwordText = findViewById<TextView>(R.id.prof_password)
-        findViewById<TextView>(R.id.prof_connexion_button).setOnClickListener {
-            username = pseudoText.toString()
-            password = passwordText.toString()
-            // requete
-        }
     }
 
     private fun setupCross(tagFragment: String?) {
--- app/src/main/java/fr/x/grama/fragments/CreateAccountFragment.kt
+++ app/src/main/java/fr/x/grama/fragments/CreateAccountFragment.kt
@@ -1,8 +1,6 @@
 package fr.x.grama.fragments
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,20 +9,20 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import fr.x.grama.R
 
-
-class ProfFragment : Fragment() {
+class CreateAccountFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val current = inflater.inflate(R.layout.fragment_profil, container, false)
-        current.findViewById<TextView>(R.id.prof_create_account).setOnClickListener {
+        val current = inflater.inflate(R.layout.fragment_profil_create, container, false)
+        current.findViewById<TextView>(R.id.prof_connexion_account_create).setOnClickListener {
             val containerLoad = requireActivity().supportFragmentManager.beginTransaction()
-            containerLoad.replace(R.id.setting_box, CreateAccountFragment())
+            containerLoad.replace(R.id.setting_box, ProfFragment())
             containerLoad.addToBackStack(null)
             containerLoad.commit()
             return@setOnClickListener
         }
         current.findViewById<Button>(R.id.prof_connexion_button).setOnClickListener {
-            val pseudoText = current.findViewById<TextView>(R.id.prof_pseudo).text.toString()
-            val passwordText = current.findViewById<TextView>(R.id.prof_password).text.toString()
+            val emailText = current.findViewById<TextView>(R.id.prof_email_create).text.toString()
+            val pseudoText = current.findViewById<TextView>(R.id.prof_pseudo_create).text.toString()
+            val passwordText = current.findViewById<TextView>(R.id.prof_password_create).text.toString()
             // requete
         }
         return current
